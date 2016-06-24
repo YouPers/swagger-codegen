e4a37c6f3e6d9877264f603b923775d5050024f6
irig <ivan.rigamonti@youpers.com>
2016-06-24 19:32:45 +0200

    first version with completely swagger generated sync layer

--- output/zocAALo/bin/android-loc8r-volley.sh
+++ output/zocAALo/bin/android-loc8r-volley.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 java -cp output/zocAALo/target/AndroidSyncClient-swagger-codegen-1.0.0.jar:modules/swagger-codegen-cli/target/swagger-codegen-cli.jar io.swagger.codegen.Codegen generate \
   -l AndroidSyncClient \
+  -c output/zocAALo/bin/android-loc8r-volley.json \
   -i /Volumes/SaSSDT1/Development/zocAALo/loc8r/app_api/doc/swagger.json \
   -o /Volumes/SaSSDT1/Development/zocAALo/Android/Swagger/loc8r-api \
   -t output/zocAALo/src/main/resources/AndroidSyncClientCodegen/volley
